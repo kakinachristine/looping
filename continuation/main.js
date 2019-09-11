fddba2fe9af6b22a637584b66da8ecc92b2e1e18
kristinekakina <bkamweti065@gmail.com>
2019-09-11 13:03:03 +0300

    this is continuation of JQuery

--- continuation/main.js
+++ continuation/main.js
@@ -28,6 +28,28 @@ $(document).ready(function(){
     });
     $("#show").dblclick(function(){
         $(".lead").toggle();
-    })
+    });
+    $(".chainBtn").click(function(){
+        $(".chaining")
+        .css("color","green")
+        .slideUp(2000)
+        .slideDown(2000,function(){
+            $(".chaining")
+            .css("background-color","pink")
 
-    })
+            
+        });
+        
+    });
+    $("#showtext").click(function(){
+        alert("THE TEXT:" +$("#test").text());
+    });
+    
+    $("#showhtml").click(function(){
+        alert("SHOW ME THE CODE:" + $("#test").html())
+    });
+    $("#showval").click(function(){
+        alert("SHOW ME THE CODE:" + $("#text").val())
+    });
+
+})
