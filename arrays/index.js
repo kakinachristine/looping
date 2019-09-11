aa6ba4c007ce647a3abf861d063a1cfe56b92d75
kristinekakina <bkamweti065@gmail.com>
2019-09-11 13:12:54 +0300

    arrays

---
+++ arrays/index.js
@@ -0,0 +1,27 @@
+var fruit1="Banana";
+var fruit2="Avacado";
+var fruit3="Apple";
+
+
+
+
+var fruits=["Banana","Avacado","Apple"];
+console.log(fruits);
+//accessing array items
+console.log(fruit1)
+var b = fruits[0]
+console.log(b);
+
+//changing/replacing array items
+fruits[1] = "Pineapple";
+console.log(fruits);
+
+//array methods and properties
+console.log (fruits.length);
+fruits.sort();
+console.log(fruits);
+//printing the last item
+fruits.push("mango");
+fruits.pop()
+console.log(fruits);
+
